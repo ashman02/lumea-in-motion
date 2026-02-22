5c1a2f81d0671317a8f53d791633d21ba4908d35
cforChobar <sidhuashman02@gmail.com>
2026-02-22 08:01:47 +0530

    services section animation is completed. Also added a utility file for common animations like heading animation

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,22 @@ import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import { SplitText } from "gsap/SplitText";
 import ServicesSectionCard from "./components/ServicesSectionCard";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { headingAnimationFunction } from "./utils/gsapAnim";
 
-gsap.registerPlugin(useGSAP, SplitText);
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
 
 export default function Home() {
+    const heroSectionRef = useRef<HTMLDivElement>(null);
     const heroHeadingRef = useRef<HTMLHeadingElement>(null);
     const heroBtnRef = useRef<HTMLAnchorElement>(null);
+
+    const servicesSectionRef = useRef<HTMLDivElement>(null);
+    const servicesHeadingRef = useRef<HTMLHeadingElement>(null);
+    const servicesWrapperRef = useRef<HTMLDivElement>(null);
+    const servicesContainerRef = useRef<HTMLDivElement>(null);
+    const servicesExtraRef = useRef<HTMLDivElement>(null);
+
     useGSAP(() => {
         // Hero Section Animations
         SplitText.create(heroHeadingRef.current, {
@@ -43,11 +53,55 @@ export default function Home() {
                 });
             },
         });
+
+        // Services Section Animations
+        headingAnimationFunction(
+            servicesHeadingRef.current,
+            servicesSectionRef.current,
+        );
+
+        const mm = gsap.matchMedia();
+        mm.add(
+            {
+                isDesktop: "(min-width: 1024px)",
+                isTablet: "(min-width: 768px) and (max-width: 1023px)",
+                isMobile: "(max-width: 767px)",
+            },
+            (context) => {
+                const { isDesktop, isTablet } = context.conditions as {
+                    isDesktop: boolean;
+                    isTablet: boolean;
+                    isMobile: boolean;
+                };
+
+                const getScrollAmountY = () => {
+                    if (!servicesContainerRef.current) return 0;
+                    const padding = isDesktop ? 128 : isTablet ? 64 : 48;
+                    const value =
+                        servicesContainerRef.current.scrollWidth +
+                        padding -
+                        window.innerWidth;
+                    return value;
+                };
+
+                gsap.to(servicesContainerRef.current, {
+                    x: () => -getScrollAmountY(),
+                    scrollTrigger: {
+                        trigger: servicesWrapperRef.current,
+                        start: isDesktop ? "bottom bottom" : "top top",
+                        end: () => `+=${getScrollAmountY()}`,
+                        pin: true,
+                        scrub: true,
+                        invalidateOnRefresh: true,
+                    },
+                });
+            },
+        );
     }, []);
 
     return (
         <main>
-            <section className="hero-section">
+            <section ref={heroSectionRef} className="hero-section">
                 <div className="relative flex h-screen min-h-125 w-full items-center justify-center">
                     <div className="Image-div absolute top-0 right-0 bottom-0 left-0">
                         <Image
@@ -82,13 +136,27 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-            <section className="about-section section-container">
-                <div className="main-container vertical-flex items-center">
-                    <div className="heading-container max-w-md overflow-hidden text-center md:max-w-150 lg:max-w-4xl">
-                        <h2 className="heading-2">{homeData.about.heading}</h2>
+            <section
+                ref={servicesSectionRef}
+                className="services-section section-container"
+            >
+                <div
+                    ref={servicesWrapperRef}
+                    className="main-container vertical-flex items-center"
+                >
+                    <div className="heading-container overflow-hidden">
+                        <h2
+                            ref={servicesHeadingRef}
+                            className="heading-2 max-w-md text-center md:max-w-150 lg:max-w-4xl"
+                        >
+                            {homeData.about.heading}
+                        </h2>
                     </div>
-                    <div className="services-container">
-                        <div className="flex w-fit gap-4 md:gap-5 lg:gap-6">
+                    <div ref={servicesExtraRef} className="">
+                        <div
+                            ref={servicesContainerRef}
+                            className="flex gap-4 md:gap-5 lg:gap-6"
+                        >
                             {homeData.about.treatments.map((t) => (
                                 <ServicesSectionCard key={t.id} treatment={t} />
                             ))}
@@ -96,6 +164,7 @@ export default function Home() {
                     </div>
                 </div>
             </section>
+            <section className="h-screen bg-bg-secondary"></section>
         </main>
     );
 }
---
+++ app/utils/gsapAnim.ts
@@ -0,0 +1,26 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+// this is function is only for heading animation of all sections
+const headingAnimationFunction = (
+    headingElement: HTMLHeadingElement | null,
+    triggerSection: HTMLDivElement | null,
+) => {
+    const serviceHeadingSplit = SplitText.create(headingElement, {
+        type: "lines",
+    });
+    gsap.from(serviceHeadingSplit.lines, {
+        y: "100%",
+        opacity: 0,
+        stagger: 0.1,
+        scrollTrigger: {
+            trigger: triggerSection,
+            start: "top 70%",
+        },
+    });
+};
+
+export {headingAnimationFunction}
